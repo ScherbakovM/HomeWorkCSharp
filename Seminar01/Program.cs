46676dfc2857bc6a3432acad5ed961d13693bbe2
Михаил <109952823+ScherbakovM@users.noreply.github.com>
2022-10-20 14:25:28 +0300

    semaniraOne

---
+++ Seminar01/Program.cs
@@ -0,0 +1,103 @@
+﻿//Домашняя работа задание 1 наибольшее из двух чисел
+
+// Console.WriteLine("Введите первое число : ");
+// int numberFirst = Convert.ToInt32(Console.ReadLine( ));
+// Console.WriteLine("Введите второе число : ");
+// int numberSecond = Convert.ToInt32(Console.ReadLine( ));
+
+// if ( numberFirst > numberSecond)
+// {
+//     Console.Write($"Наибольшее число {numberFirst}");
+// }
+
+// else {
+//     Console.Write($"Наибольшее число {numberSecond}");
+// }
+
+//Домашняя работа задание 2  наибольшее из 3 чисел
+
+// Console.WriteLine("Введите первое число : ");
+// int numberFirst = Convert.ToInt32(Console.ReadLine( ));
+// Console.WriteLine("Введите второе число : ");
+// int numberSecond = Convert.ToInt32(Console.ReadLine( ));
+// Console.WriteLine("Введите третье число : ");
+// int numberThird = Convert.ToInt32(Console.ReadLine( ));
+
+// if ( numberFirst > numberSecond)
+// {
+//     Console.Write($"Наибольшее число {numberFirst}");
+// }
+
+// else if(numberSecond > numberThird)
+// {
+//     Console.Write($"Наибольшее число {numberSecond}");   
+// }
+
+// else {
+//     Console.Write($"Наибольшее число {numberThird}");
+// }
+
+//  Домашняя работа Задание 3
+
+// Console.WriteLine("Введите число : ");
+// int numberFirst = Convert.ToInt32(Console.ReadLine( ));
+// int count = numberFirst % 2;
+
+// if (count == 0)
+
+//     Console.Write($" {numberFirst}  чётное число");
+
+// else 
+
+//    Console.Write($" {numberFirst}  нечётное число");
+
+
+//  Домашняя работа Задание 4
+
+//  Console.WriteLine("Введите ваше число : ");
+// int N = Convert.ToInt32(Console.ReadLine( ));
+
+// for (int EvenNumber = 2; EvenNumber <= N; EvenNumber = EvenNumber + 2)
+// Console.Write(EvenNumber);
+
+//  int Max(int arg1, int arg2, int arg3) 
+// {
+// int result = arg1;
+// if (arg2 > result) result =arg2;
+// if (arg3 > result) result =arg3;
+// return result;
+// }
+
+
+// int a1 = 5;
+// int b1 = 6;
+// int c1 = 8;
+
+// int a2 = 666;
+// int b2 = 21;
+// int c2 = 110;
+
+// int a3 = 3;
+// int b3 = 21;
+// int c3 = 11;
+
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int MAXRESULT = Max(max1, max2, max3);
+
+
+
+// int MAXRESULT = Max(
+//     Max(a1, b1, c1),
+//     Max(a2, c2, b2),
+//     Max(c3, b3, a3));
+
+
+// Console.Write(MAXRESULT);
+
+
+
+
+
