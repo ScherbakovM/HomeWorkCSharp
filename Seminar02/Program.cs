97741114a5dbfba1538fdc251075384bdc39c8e9
Михаил <109952823+ScherbakovM@users.noreply.github.com>
2022-10-20 15:53:57 +0300

    task1

---
+++ Seminar02/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("enter you three-digit nubers:");
+ int threeDigit = Convert.ToInt32(Console.ReadLine());
+
+ int FindNum2(int num)
+ {
+  int Num2 = (num % 100) / 10;
+  return Num2;
+ }
+
+int Num2 = FindNum2(threeDigit);
+Console.WriteLine($"second number your three-digital numer {Num2}");
