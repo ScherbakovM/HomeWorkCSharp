9b620e519e1707dae5f3b29dc7a43d0e62bb67ac
Mikhail <miwanya.spb@gmail.com>
2022-11-30 06:22:15 +0300

    check

--- Seminar09/Program.cs
+++ Seminar09/Program.cs
@@ -1,16 +1,4 @@
 ﻿
-// double SumofDigits(double n, double b)
-// {   
-//     if(b > 0) 
-//     return SumofDigits(n, b - 1 ) * n;
-//     else if(b < 0)
-//     return SumofDigits(n, b + 1) / n;
-    
-//     return 1;
-// }
-
-// Console.WriteLine(SumofDigits(2,-3));
-
 // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 // int NaturalNumber(int N)
@@ -42,9 +30,6 @@
 // Console.Write(SummDigit(1, 15));
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
-
 // int A(int m, int n)
 // {
 //   if (m == 0)
